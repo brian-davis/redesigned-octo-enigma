d8edd3f0c8e7c6b444268f997b0f03ffc34264d2
Brian Davis <gbriandavis@fastmail.com>
2023-08-25 12:35:28 -0700

    set up

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Post.delete_all
+10.times do
+  title = Faker::Lorem.sentence( word_count: 6 )
+  body = 100.times.map {
+    "<p>#{Faker::Lorem.paragraph( sentence_count: 30 )}</p>"
+  }.join("\n")
+  Post.create({
+    title: title,
+    body: body
+  })
+end
